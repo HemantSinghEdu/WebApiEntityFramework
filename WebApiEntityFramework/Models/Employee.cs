af7b93be49d48be95c8d5de84ce00afd71e0643b
singh.hemant.edu@hotmail.com <singh.hemant.edu@hotmail.com>
2023-08-30 22:29:26 -0500

    Made employee id nullable to fix "Employee Id is required field" error when trying to add a new employee

--- WebApiEntityFramework/Models/Employee.cs
+++ WebApiEntityFramework/Models/Employee.cs
@@ -2,7 +2,7 @@
 {
     public class Employee
     {
-        public string EmployeeId { get; set; }
+        public string? EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; } 
         public DateTime DateOfBirth { get; set; }
